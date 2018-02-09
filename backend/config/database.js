5ef07570aa6da5afdf158c83f9a6b9ab5f95069e
Tadeu Espíndola Palermo <tadeupalermoti@gmail.com>
2018-02-09 21:18:49 -0200

    Personalizei mensagens do tipo required no arquivo de configuração da conexão com o Banco de Dados

--- backend/config/database.js
+++ backend/config/database.js
@@ -9,3 +9,6 @@ const mongoose = require('mongoose')
 // Startar o mongo antes no console: sudo service mongod start (para Linux)
 // Neste caso, não está sendo utilizado usuário e senha para o Banco
 module.exports = mongoose.connect('mongodb://localhost/db_finance')
+
+// Qualquer mensagem do tipo required seguirá esse padrão de escrita
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório!"
