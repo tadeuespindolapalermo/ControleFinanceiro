6635885240445f98cb1591c8998faffb1e67f51e
Tadeu Espíndola Palermo <tadeupalermoti@gmail.com>
2018-02-09 19:23:45 -0200

    Refatorei a implementaçaõ do Servidor com Express e criei a conexão com o Banco de Dados MongoDB

---
+++ backend/config/database.js
@@ -0,0 +1,11 @@
+/**
+ * Configuração da conexão com o Banco de Dados MongoDB
+ */
+
+// Incluindo a dependência do Mongo
+const mongoose = require('mongoose')
+
+// Conexão
+// Startar o mongo antes no console: sudo service mongod start (para Linux)
+// Neste caso, não está sendo utilizado usuário e senha para o Banco
+module.exports = mongoose.connect('mongodb://localhost/db_finance')
--- backend/loader.js
+++ backend/loader.js
@@ -1,43 +1,5 @@
-// Back-end rodará na porta 3003.
-const port = 3003
+// Chamada do servidor
+require('./config/server')
 
-/**
- * Dependências
- */
-// Possui funções middleware para fazer a interpretação do corpo da requisição (parse do body).
-const bodyParser = require('body-parser')
-
-// Incluindo o Framework Web Express para back-end.
-const express = require('express')
-
-// Criando o servidor iniciando o express.
-const server = express()
-
-// Urlencoded é o formato dos dados obtidos quando é feito a submissão de um formulário no front-end.
-// Extended true é capaz de interpretar variados tipos de informações que venham na submissão de um formulário.
-// Body parser é responsável por fazer a interpretação dos dados que venham a partir da submissão de um formulário.
-// Use: toda requisição que chegar no back-end será passada pelo middleware urlencoded.
-server.use(bodyParser.urlencoded({ extended: true }))
-
-// Analisa se o corpo da requisição é um conteúdo json, se for, faz o parser transformando o json em um objeto para ser usado no back-end.
-server.use(bodyParser.json())
-
-// Servidor escutando a porta do back-end, caso não esteja sendo usada.
-// Chamar no console: npm run dev
-server.listen(port, function() {
-    console.log(`BACKEND is running  on port ${port}.`)
-})
-
-/*
-// Função própria que serve como Middleware
-// Recebe requisição, resposta e o next como parâmetro
-// Next é a cadeia de chamadas de um middleware para o outro
-server.use(function(req, res, next) {
-    console.log('Middleware 1')
-    next()
-})
-
-server.use(function(req, res, next) {
-    console.log('Middleware 2')
-    res.send('Funcionou!!!')
-})*/
+// Chamada do MongoDB
+require('./config/database')
