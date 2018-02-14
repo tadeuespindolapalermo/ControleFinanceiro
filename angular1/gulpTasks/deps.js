f9f0bc1074b4f6ddb4bd4e0103e55969490febc5
Tadeu Espíndola Palermo <tadeupalermoti@gmail.com>
2018-02-13 22:10:49 -0200

    Adicionei Implementação do Build dos Arquivos das Dependências do Projeto

--- angular1/gulpTasks/deps.js
+++ angular1/gulpTasks/deps.js
@@ -3,18 +3,50 @@
  */
 
 const gulp = require('gulp')
+const uglify = require('gulp-uglify')
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
 
 // [Tasks que são pré-requisito] para a task (deps)
 gulp.task('deps', ['deps.js', 'deps.css', 'deps.fonts'])
 
 gulp.task('deps.js', () => {
-
+    // Array de arquivos JS que são dependências do projeto
+    return gulp.src([
+    'node_modules/angular/angular.min.js',
+    'node_modules/angular-ui-router/release/angular-ui-router.min.js',
+    'node_modules/angular-animate/angular-animate.min.js',
+    'node_modules/angular-toastr/dist/angular-toastr.tpls.min.js',
+    'node_modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
+    'node_modules/admin-lte/bootstrap/js/bootstrap.min.js',
+    'node_modules/admin-lte/plugins/slimScroll/jquery.slimscroll.min.js',
+    'node_modules/admin-lte/dist/js/app.min.js',
+  ])
+  // Reduz e otimiza o arquivo
+  .pipe(uglify())
+  // Gera um arquivo único com todas as dependências, depois deles terem passado pelo uglify
+  .pipe(concat('deps.min.js'))
+  // Joga o arquivo minificado e agrupado na posta declarada por parâmetro
+  .pipe(gulp.dest('public/assets/js'))
 })
 
 gulp.task('deps.css', () => {
-
+    return gulp.src([
+    'node_modules/angular-toastr/dist/angular-toastr.min.css',
+    'node_modules/font-awesome/css/font-awesome.min.css',
+    'node_modules/admin-lte/bootstrap/css/bootstrap.min.css',
+    'node_modules/admin-lte/dist/css/AdminLTE.min.css',
+    'node_modules/admin-lte/dist/css/skins/_all-skins.min.css',
+  ])
+  .pipe(uglifycss({ "uglyComments": true }))
+  .pipe(concat('deps.min.css'))
+  .pipe(gulp.dest('public/assets/css'))
 })
 
 gulp.task('deps.fonts', () => {
-
+    return gulp.src([
+    'node_modules/font-awesome/fonts/*.*',
+    'node_modules/admin-lte/bootstrap/fonts/*.*'
+  ])
+  .pipe(gulp.dest('public/assets/fonts'))
 })
