2368861c7eac909c8853084006ea449be23bfb73
Tadeu Espíndola Palermo <tadeupalermoti@gmail.com>
2018-02-15 20:01:26 -0200

    Implementei o Componente de Cabeçalho de Conteúdo

---
+++ angular1/app/common/components/contentHeader.js
@@ -0,0 +1,12 @@
+// Componente de Cabeçalho de Conteúdo
+angular.module('app').component('contentHeader', {
+    bindings: {
+        name: '@',
+        small: '@',
+    },
+    template: `
+    <section class="content-header">
+        <h1>{{ $ctrl.name }} <small>{{ $ctrl.small }}</small></h1>
+    </section>
+    `
+})
