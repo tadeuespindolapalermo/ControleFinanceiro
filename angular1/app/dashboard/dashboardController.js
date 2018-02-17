79066d648a1cc72e3c46c4636606e9519c692cac
Tadeu Espíndola Palermo <tadeupalermoti@gmail.com>
2018-02-17 21:17:43 -0200

    Atualização do Controller do Dashboard para Angular 1.6

--- angular1/app/dashboard/dashboardController.js
+++ angular1/app/dashboard/dashboardController.js
@@ -1,17 +1,20 @@
-angular.module('app').controller('DashboardCtrl', [
-    '$scope',
-    '$http',
-    DashboardController
-])
+(function() {
+    angular.module('app').controller('DashboardCtrl', [
+        '$http',
+        DashboardController
+    ])
 
-function DashboardController($scope, $http) {
-    $scope.getSummary = function() {
-        const url = 'http://localhost:3003/api/billingSummary'
-        $http.get(url).success(function({credit = 0, debt = 0}) {
-            $scope.credit = credit
-            $scope.debt = debt
-            $scope.total = credit - debt
-        })
+    function DashboardController($http) {
+        const vm = this
+        vm.getSummary = function() {
+            const url = 'http://localhost:3003/api/billingSummary'
+            $http.get(url).then(function(response) {
+                const {credit = 0, debt = 0} = response.data
+                vm.credit = credit
+                vm.debt = debt
+                vm.total = credit - debt
+            })
+        }
+        vm.getSummary()
     }
-    $scope.getSummary()
-}
+})()
