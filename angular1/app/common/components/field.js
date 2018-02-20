dbd4f577241430c6a1c078fa4e4a1ee45748357f
Tadeu Espíndola Palermo <tadeupalermoti@gmail.com>
2018-02-20 00:14:20 -0300

    Criei componente Campo de Formulário (Field).

---
+++ angular1/app/common/components/field.js
@@ -0,0 +1,26 @@
+(function() {
+    angular.module('app').component('field', {
+        bindings: {
+            id: '@',
+            label: '@',
+            grid: '@',
+            placeholder: '@',
+            type: '@',
+        },
+        controller: [
+            'gridSystem',
+            function(gridSystem) {
+                this.gridClasses = gridSystem.toCssClasses(this.grid)
+            }
+        ],
+        template: `
+         <div class="{{ $ctrl.gridClasses }}">
+             <div class="form-group">
+                 <label for="{{ $ctrl.id }}">{{ $ctrl.label }}</label>
+                 <input id="{{ $ctrl.id }}" class="form-control" placeholder="{{ $ctrl.placeholder }}"
+                 type="{{ $ctrl.type }}" />
+             </div>
+         </div>
+        `
+     });
+})()
