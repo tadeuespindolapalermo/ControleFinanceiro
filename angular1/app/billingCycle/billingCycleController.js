4abdf4babe998d8e184d205d0f5a7d0ea56e5d19
Tadeu Espíndola Palermo <tadeupalermoti@gmail.com>
2018-02-20 01:49:56 -0300

    Implementei o Factory de exibição de mensagens ao usuário!

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -1,17 +1,20 @@
 (function() {
     angular.module('app').controller('BillingCycleCtrl', [
         '$http',
+        'msgs',
         BillingCycleController
     ])
 
-    function BillingCycleController($http) {
+    function BillingCycleController($http, msgs) {
         const vm = this
 
         vm.create = function () {
             const url = 'http://localhost:3003/api/billingCycles'
             $http.post(url, vm.billingCycle).then(function(response) {
                 vm.billingCycle = {}
-                console.log('Sucesso!')
+                msgs.addSuccess('Operação realizada com sucesso!!')
+            }).catch(function(resp) {                                
+                msgs.addError(resp.data.errors)
             })
         }
     }
