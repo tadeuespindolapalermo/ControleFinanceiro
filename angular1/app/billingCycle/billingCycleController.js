a5f23c285f50e7a34410edc505ebf2a682ea432b
Tadeu Espíndola Palermo <tadeupalermoti@gmail.com>
2018-02-27 22:09:54 -0300

    Modificações no código para atender a implementação do AngularJS na versão 1.6.1.

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -16,7 +16,7 @@
             $http.get(`${url}?skip=${(page - 1) * 10}&limit=10`).then(function(response) {
                 vm.billingCycle = {credits: [{}], debts: [{}]}
                 vm.billingCycles = response.data
-                vm.calculateValues()                
+                vm.calculateValues()
 
                 $http.get(`${url}/count`).then(function(response) {
                     vm.pages = Math.ceil(response.data.value / 10)
@@ -29,8 +29,8 @@
             $http.post(url, vm.billingCycle).then(function(response) {
                 vm.refresh()
                 msgs.addSuccess('Operação realizada com sucesso!!')
-            }).catch(function(resp) {
-                msgs.addError(resp.data.errors)
+            }).catch(function(response) {
+                msgs.addError(response.data.errors)
             })
         }
 
@@ -45,8 +45,8 @@
             $http.put(updateUrl, vm.billingCycle).then(function(response) {
                 vm.refresh()
                 msgs.addSuccess('Operação realizada com sucesso!')
-            }).catch(function(resp) {
-                msgs.addError(resp.data.errors)
+            }).catch(function(response) {
+                msgs.addError(response.data.errors)
             })
         }
 
@@ -61,8 +61,8 @@
             $http.delete(deleteUrl, vm.billingCycle).then(function(response) {
                 vm.refresh()
                 msgs.addSuccess('Operação realizada com sucesso!')
-            }).catch(function(resp) {
-                msgs.addError(resp.data.errors)
+            }).catch(function(response) {
+                msgs.addError(response.data.errors)
             })
         }
 
