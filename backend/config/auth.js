deb0e4f4fec1dcb5a0724c9a1ea0e38f3ad91c20
Tadeu Espíndola Palermo <tadeupalermoti@gmail.com>
2018-03-01 20:51:23 -0300

    Implementação da proteção da API (validação o token JWT para as rotas protegidas e garantia de que a API esteja protegida. Habilitação do Authorization no CORS.

---
+++ backend/config/auth.js
@@ -0,0 +1,34 @@
+/**
+ * Middleware responsável por proteger a API
+ * Esse [middleware] será o responsável por validar o token JWT para as routas protegidas
+ * e garantir que a API esteja protegida.
+ */
+
+// Importações necessárias
+const jwt = require('jsonwebtoken')
+const env = require('../.env')
+
+module.exports = (req, res, next) => {
+
+    // CORS preflight request
+    if(req.method === 'OPTIONS') {
+        next()
+    } else {
+        const token = req.body.token || req.query.token || req.headers['authorization']
+
+        if(!token) {
+            return res.status(403).send({errors: ['No token provided.']})
+        }
+
+        jwt.verify(token, env.authSecret, function(err, decoded) {
+            if(err) {
+                return res.status(403).send({
+                    errors: ['Failed to authenticate token.']
+                })
+            } else {
+                // req.decoded = decoded
+                next()
+            }
+        })
+    }
+}
