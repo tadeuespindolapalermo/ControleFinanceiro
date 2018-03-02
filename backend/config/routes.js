e569c5d9fca133c0f0d97597314372eed57facf5
Tadeu Espíndola Palermo <tadeupalermoti@gmail.com>
2018-03-01 21:07:13 -0300

    Alterei as rotas da aplicação para o funcionamento da implementação de segurança na aplicação através de autenticação no back-end.

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -5,20 +5,50 @@
 // Inclusão da dependência express
 const express = require('express')
 
+const auth = require('./auth')
+
 // Em cima do server será definido as rotas, então server deve ser passado como parâmetro
 module.exports = function(server) {
 
+     /*
+      * Rotas abertas
+      */
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+
+    const AuthService = require('../api/user/authService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
+
+    /*
+     * Rotas protegidas por Token JWT
+     */
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
+
+    protectedApi.use(auth)
+
+    const billingCycleService = require('../api/billingCycle/billingCycleService')
+    billingCycleService.register(protectedApi, '/billingCycles')
+
+    const billingSummaryService = require('../api/billingSummary/billingSummaryService')
+    protectedApi.route('/billingSummary').get(billingSummaryService.getSummary)
+
+    /**
+     * Implementação para uso da aplicação sem autenticação
+     */
     // API Routes
     // Tudo que vier de /api será passado para o Middleware router
-    const router = express.Router()
-    server.use('/api', router)
+    // const router = express.Router()
+    // server.use('/api', router)
 
     // rotas da API
-    const billingCycleService = require('../api/billingCycle/billingCycleService')
+    // const billingCycleService = require('../api/billingCycle/billingCycleService')
     // Todos os serviços usará como url raiz /billingCycles
-    billingCycleService.register(router, '/billingCycles')
+    // billingCycleService.register(router, '/billingCycles')
 
     // /api/billingSummary
-    const billingSummaryService = require('../api/billingSummary/billingSummaryService')
-    router.route('/billingSummary').get(billingSummaryService.getSummary)
+    // const billingSummaryService = require('../api/billingSummary/billingSummaryService')
+    // router.route('/billingSummary').get(billingSummaryService.getSummary)
 }
