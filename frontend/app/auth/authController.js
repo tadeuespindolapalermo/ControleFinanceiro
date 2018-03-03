eda1f6dacadd30df29449041ed47053a336a7445
Tadeu Espíndola Palermo <tadeupalermoti@gmail.com>
2018-03-03 20:53:52 -0300

    Implementei a estrutura básica das telas de Login e Cadastro no Front-end.

--- frontend/app/auth/authController.js
+++ frontend/app/auth/authController.js
@@ -10,11 +10,25 @@
 
         const vm = this
 
+        vm.loginMode = true
+
+        vm.changeMode = () => vm.loginMode = !vm.loginMode
+
+        vm.login = () => {
+            //auth.login(vm.user, err => err ? msgs.addError(err) : $location.path('/'))
+            console.log(`Login... ${vm.user.email}`)
+        }
+
+        vm.signup = () => {
+            //auth.signup(vm.user, err => err ? msgs.addError(err) : $location.path('/'))
+            console.log(`Signup... ${vm.user.email}`)
+        }
+
         vm.getUser = () => ({ name: 'Usuário MOCK', email: 'mock@cod3r.com.br' })
 
         vm.logout = () => {
             console.log('Logout...')
         }
     }
-    
+
 })()
